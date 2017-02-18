7389e69ce833abb321775409ce7ef1f6938dc24f
jedrzej <jedrzej@szczepaniak.com>
2017-02-18 20:54:03 +0100

    task done

--- Algorithms/Implementation/library-fine/library_fine.py
+++ Algorithms/Implementation/library-fine/library_fine.py
@@ -1,8 +1,18 @@
 from collections import namedtuple
 Date = namedtuple('Date', 'day month year')
 
-def calculate_fine(due_date, ret_day):
-    if due_date.year > ret_day.year:
-        if due_date.month >= ret_day.month:
-            if due_date.day >= ret_day.day:
+def calculate_fine(due_date, ret_date):
+    if due_date.year == ret_date.year:
+        if due_date.month == ret_date.month:
+            if due_date.day >= ret_date.day:
                 return 0
+            else:
+                return (ret_date.day - due_date.day) * 15
+        elif due_date.month > ret_date.month:
+            return 0
+        else:
+            return (ret_date.month - due_date.month) * 500
+    elif due_date.year > ret_date.year:
+        return 0
+    else:
+        return 10000
--- Algorithms/Implementation/library-fine/library_fine_test.py
+++ Algorithms/Implementation/library-fine/library_fine_test.py
@@ -3,14 +3,46 @@ from library_fine import Date
 import unittest
 
 class TestLibraryFine(unittest.TestCase):
+    def dates_test(self, dates):
+        for ret_date, due_date, fine in dates:
+            self.assertEqual(
+                calculate_fine(
+                    due_date=due_date, ret_date=ret_date),
+                    fine,
+                    "\nreturn date: {},\ndue date: {},\nexpected fine: {}.".format(ret_date, due_date, fine))
+
     def test_no_fine_charged(self):
         dates = [
+                    # return date, due date, fine
                     (Date(6 ,6, 2015), Date(9, 6, 2015), 0),
                     (Date(9 ,6, 2015), Date(9, 6, 2015), 0),
                     (Date(10 ,6, 2014), Date(9, 6, 2015), 0),
-
+                    (Date(10 ,2, 2015), Date(9, 6, 2015), 0),
                 ]
-        for ret_day, due_date, fine in dates:
-            self.assertEqual(calculate_fine(due_date=due_date, ret_day=ret_day), fine)
+        self.dates_test(dates)
+
+    def test_fine_charged_in_days(self):
+        dates = [
+                    # return date, due date, fine
+                    (Date(10 ,6, 2015), Date(9, 6, 2015), 1*15),
+                    (Date(29 ,6, 2015), Date(9, 6, 2015), 20*15),
+                ]
+        self.dates_test(dates)
+
+    def test_fine_charged_in_month(self):
+        dates = [
+                    # return date, due date, fine
+                    (Date(10 ,7, 2015), Date(9, 6, 2015), 1*500),
+                    (Date(29 ,12, 2015), Date(9, 6, 2015), 6*500),
+                ]
+        self.dates_test(dates)
+
+    def test_fine_charged_in_years(self):
+        dates = [
+                    # return date, due date, fine
+                    (Date(10 ,6, 2016), Date(9, 6, 2015), 10000),
+                    (Date(29 ,6, 2017), Date(9, 6, 2015), 10000),
+                ]
+        self.dates_test(dates)
 if __name__ == '__main__':
     unittest.main()
