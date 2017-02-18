2bb0a1a92b9a3634acc500e9567f830da71bd468
jedrzej <jedrzej@szczepaniak.com>
2017-02-18 11:02:09 +0100

    fixes

--- Algorithms/Implementation/append-and-delete/append_and_delete.py
+++ Algorithms/Implementation/append-and-delete/append_and_delete.py
@@ -7,17 +7,17 @@ def common_prefix_len(first,second):
             break
     return pref_len
 
-def is_convertable_in_k_steps(s,t,k):
+def is_convertable(s,t,steps):
     pref_len = common_prefix_len(s,t)
     diff_without_pref = len(s) + len(t) - 2 * pref_len
 
-    if k % 2 == diff_without_pref % 2:
-        if k >= diff_without_pref:
+    if steps % 2 == diff_without_pref % 2:
+        if steps >= diff_without_pref:
             return True
         else:
             return False
     else:
-        if k >= len(s) + len(t):
+        if steps >= len(s) + len(t):
             return True
         else:
             return False
--- Algorithms/Implementation/append-and-delete/append_and_delete_test.py
+++ Algorithms/Implementation/append-and-delete/append_and_delete_test.py
@@ -1,5 +1,5 @@
 import unittest
-from append_and_delete import is_convertable_in_k_steps
+from append_and_delete import is_convertable
 
 class TestAppendAndDelete(unittest.TestCase):
     def test_basic_input(self):
@@ -7,15 +7,22 @@ class TestAppendAndDelete(unittest.TestCase):
                     ('hackerhappy', 'hackerrank', 9, True),
                     ('hackerhappy', 'hackerrank', 10, False),
                     ('hackerhappy', 'hackerrank', 11, True),
+
+                    ('alalala', 'alalala', 0, True),
                     ('alalala', 'alalala', 3, False),
-                    ('alalala', 'alalala', 7, True),
-                    ('alalala', 'alalala', 8, False),
-                    ('alalala', 'alalala', 9, True),
+                    ('alalala', 'alalala', 7, False),
+                    ('alalala', 'alalala', 8, True),
+                    ('alalala', 'alalala', 9, False),
+
+                    ('alalala', 'alalala', 14, True),   #From len(s) + len(t) steps, odd and even number of steps are possible
+                    ('alalala', 'alalala', 15, True),
+
                     ('abcde', 'abcdf', 1, False),
                     ('abcde', 'abcdf', 2, True),
+
                 ]
         for s,t,k, expected in tests:
-            self.assertEqual(is_convertable_in_k_steps(s,t,k),expected)
+            self.assertEqual(is_convertable(s,t,steps=k), expected, 'convertion {} -> {} in {}, steps'.format(s,t,k))
 
 if __name__ == '__main__':
     unittest.main()
